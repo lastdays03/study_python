aeca447e4b18bf9c471ef07afb8ba33c66a9af4b
lastdays03 <lastdays03@gmail.com>
2025-11-21 14:53:31 +0900

    문서 업데이트: Matplotlib 및 Pandas 시각화 가이드 보강, 실습 파일 추가

---
+++ practice/commonpractice.py
@@ -0,0 +1,23 @@
+import platform
+import matplotlib as mpl
+from matplotlib import font_manager, rc
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+
+system = platform.system()
+if system == "Windows":
+    font_path = "C:/Windows/Fonts/malgun.ttf"
+elif system == "Darwin":
+    font_path = "/System/Library/Fonts/AppleSDGothicNeo.ttc"
+elif system == "Linux":
+    font_path = "/usr/share/fonts/truetype/nanum/NanumGothic.ttf"
+else:
+    font_path = None
+if font_path:
+    font = font_manager.FontProperties(fname=font_path).get_name()
+    mpl.rcParams['font.family'] = font
+else:
+    print("폰트 경로를 확인해주세요.")
+
+mpl.rcParams['axes.unicode_minus'] = False
--- report/data/test.py
+++ report/data/test.py
@@ -9,3 +9,4 @@ print(os.getcwd())
 # 데이터프레임 생성
 d1 = pd.read_excel("report/data/notExercise.xls")
 print(d1)
+
